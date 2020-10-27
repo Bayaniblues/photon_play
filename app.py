eab3ad58b0238426e22dd95f1f59e809e07cd7f7
Bayaniblues <cortez.ethridge@gmail.com>
2020-10-26 18:52:01 -0600

    test

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
 import dash
 import dash_bootstrap_components as dbc
 import dash_core_components as dcc
+import flask
 
-
+server = flask.Flask(__name__)
 app = dash.Dash(__name__,
                 external_stylesheets=[dbc.themes.BOOTSTRAP],
-                suppress_callback_exceptions=True)
-server = app.server
+                suppress_callback_exceptions=True,
+                server=server)
